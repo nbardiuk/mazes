bbbcd0c80e3903078360568edf86b7750b0a17dc
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-10-18 00:20:04 +0100

    add renders of binary_tree and sidewinder algorithms

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod maze;
 mod sidewinder;
 
 fn main() -> std::io::Result<()> {
-    let maze = Sidewinder::on(Maze::new(150, 100));
+    let maze = Sidewinder::on(Maze::new(50, 50));
     svg::save("image.svg", &maze.to_svg(10))?;
     Ok(())
 }
